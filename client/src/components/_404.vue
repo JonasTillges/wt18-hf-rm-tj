78d64f05901dffa669b61cbbc95a02b3bd2186ec
Florian Hartl <f.hartl@techdivision.com>
2019-01-16 12:10:21 +0100

    404 Page

--- client/src/components/_404.vue
+++ client/src/components/_404.vue
@@ -1,9 +1,46 @@
 <template>
-    <div id="app">
-        
+    <div>
+        <lottie :options="defaultOptions"  v-on:animCreated="handleAnimation"/>
     </div>
 </template>
 
 <script>
+    import Lottie from './lottie.vue';
+    import * as animationData from '../animations/404_page.json';
 
+    export default {
+        name: '_404',
+        components: {
+            'lottie': Lottie
+        },
+        data() {
+            return {
+                defaultOptions: {animationData: animationData},
+                animationSpeed: 1
+            }
+        },
+        methods: {
+            handleAnimation: function (anim) {
+                this.anim = anim;
+                this.anim.play();
+
+            },
+
+            stop: function () {
+                this.anim.stop();
+            },
+
+            play: function () {
+                this.anim.play();
+            },
+
+            pause: function () {
+                this.anim.pause();
+            },
+
+            onSpeedChange: function () {
+                this.anim.setSpeed(this.animationSpeed);
+            }
+        }
+    }
 </script>
